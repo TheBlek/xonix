e2965343f2470a0c03097f363bb657f7faa3f433
hikmatillo <dolgovdaniil007@gmail.com>
2023-04-11 12:54:31 +0700

    Added pushing coordinates to the stack

--- player_movement.asm
+++ player_movement.asm
@@ -73,9 +73,86 @@
         st r0, r1
     wend
 	halt
+	
+checkCoord:
+	# r0 - xCoord, r1 - yCoord
+	# dividing by 8 with remainder
+	# to find place in byte and byte itself
+	# for x coordinate
+	ldi r2, 0x07
+	and r0, r2
+	shra r0
+	shra r0
+	shra r0
 
+	ldi r3, display
+	add r3, r0
+	ldi r3, 0x80 # r3 - mask
+	
+	while
+		tst r2
+	stays nz
+		shr r3
+		dec r2	
+	wend	
+
+	shla r1
+	shla r1	
+	add r1, r0 #r0 now is the number of a byte
+	ld r0, r1 # r1 now is a byte
+	and r3, r1 # r1 now has only requested bit (i.e. 0b00000100)
 		
     wend
+
+changeMovement:
+	ldi r0, player_x # r0 - previous X
+	ld r0, r0
+	
+	ldi r1, player_x # r0 - previous Y
+	ld r1, r1
+	
+	jsr calculatePlayer
+	
+	ldi r2, player_x # r2 - previous X
+	ld r2, r2
+	
+	ldi r3, player_y # r3 - previous Y
+	ld r3, r3
+	
+	if 
+		sub r2, r0 # check if x is changed
+		tst r0
+	is nz
+		if 
+			sub r3, r1 # check if y is changed
+			tst r1
+		is nz
+			push r2 # push changed x
+			push r3 # push changed y
+		else
+			push r2 # push changed x
+			push r3 # push UNCHANGED y
+		fi
+	else # at this point x is not changed
+		if 
+			sub r3, r1 # check if y is changed
+			tst r1
+		is nz
+			push r2 # push UNCHANGED x
+			push r3 # push changed y
+		else
+			push r2 # push UNCHANGED x
+			push r3 # push UNCHANGED y
+		fi
+	fi
+	
+	
+calculatePlayer: # update player position
+	ldi r3, calculate_player 
+	st r3, 0x01
+	rts
+	
+	
 
 
 define player_offset, 0x00
