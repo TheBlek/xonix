2cb603eb72437893e9a544016ef5a7c79d0a7967
Blek <egorka99966@gmail.com>
2023-05-05 17:53:33 +0700

    Calculate ball position in hardware. Rewrite ball movement script

--- main.asm
+++ main.asm
@@ -1,4 +1,4 @@
-	asect 0x00
+    asect 0x00
  
     ldi r0, override_screen
     ldi r1, 0b1000
@@ -18,19 +18,19 @@
     ldi r0, flush
     st r0, r0
 
-	ldi r0, override_screen
+    ldi r0, override_screen
     ldi r1, 0b1011
     st r0, r1
     ldi r0, flush
     st r0, r0
 
-	ldi r0, override_screen
+    ldi r0, override_screen
     ldi r1, 0b1100
     st r0, r1
     ldi r0, flush
     st r0, r0
 
-	ldi r0, override_screen
+    ldi r0, override_screen
     ldi r1, 0b1111
     st r0, r1
     ldi r0, flush
@@ -64,14 +64,14 @@
     st r0, r1
 
     while
-        ldi r1, player_byte
+        ldi r0, calculate_player
         tst r1 # practically infinite loop
     stays nz
         # Player offset is in r0
         # Player byte is in r1
         # Load player data 
 
-        ldi r0, calculate_player
+        # Calculate new player position 
         st r0, r0
 
         ldi r0, player_offset
@@ -119,10 +119,6 @@
                 ldi r1, 0
                 ldi r3, override_screen
                 st r3, r1
-
-                ldi r3, turn_count
-                ldi r2, 0
-                st r3, r2
                 popall
             fi
         fi
@@ -134,43 +130,31 @@
         # Place background to screen
         st r0, r1
     wend
-	halt
-	
+    halt
+    
 go:
 
     pushall
 
-    ldi r0, ballAdress
-    ld r0, r0
-    ld r0, r1# в r1 окружающая среда
-    ldi r2, ball# шарик
-    ld r2, r2
+    ldi r3, forwardOrBack
+    ld r3, r0
+    ldi r2, calculate_ball_horizontal
+    st r2, r2
 
     if
-        ldi r3, forwardOrBack
-        ld r3, r3
-        tst r3
-    is z
+        ldi r0, ballAdress
+        ld r0, r0
+        ld r0, r1
 
-        shr r2
-
-    else
-
-        shl r2
-    
-    fi
-
-    if
+        ldi r2, ball
+        ld r2, r2
         or r1, r2
         cmp r1, r2 #    если шарик врезался в стену
     is eq
-
+        # Flip a flag
         if
-            push r3
-            ldi r3, forwardOrBack
             ld r3, r3
             tst r3
-            pop r3
         is z   
     
             ldi r1, 1
@@ -187,188 +171,65 @@ go:
 
         fi
 
-        ldi r3, flush
-        ldi r2, ball
-        ld r2, r2
+        # Move ball back
+        ldi r2, calculate_ball_horizontal
+        st r2, r2
+    fi
 
+    ldi r3, upOrDown
+    ld r3, r1
+    ldi r2, calculate_ball_vertical
+    st r2, r2
+
+    if
+        ldi r0, ballAdress
+        ld r0, r0
         ld r0, r1
 
-        or r1, r2
-
-        st r0, r2
-        st r3, r2
-
-        st r0, r1
-
-    else
-
-        ldi r3, flush
         ldi r2, ball
         ld r2, r2
-
-        ld r0, r1
-
         or r1, r2
-
-        st r0, r2
-        st r3, r2
-
-        st r0, r1
-
-        xor r1, r2
-
-        move r0, r1# в r1 окружающая среда
-        
-        ldi r2, diagonal
-        ld r2, r2
-
+        cmp r1, r2 #    если шарик врезался в стену
+    is eq
+        # Flip a flag
         if
-            push r3
-            ldi r3, upOrDown
             ld r3, r3
             tst r3
-            pop r3
-        is nz
+        is z  
 
-            add r2, r1
-        
-        else
-        
-            sub r1,r2
-            move r2, r1
-        
+            ldi r1, 1
+            ldi r3, upOrDown
+            st r3, r1
+
+        else 
+
+            ldi r1, 0
+            ldi r3, upOrDown
+
+            st r3, r1
+
         fi
 
-        ld r1, r1
-        ldi r2, ball# шарик
-
-        ld r2, r2
-
-        ldi r3, flush
-
-        if
-            or r1, r2
-            cmp r1, r2 #    если шарик врезался в стен            
-        is eq
-
-            if
-                push r3
-                ldi r3, upOrDown
-                ld r3, r3
-                tst r3
-                pop r3
-            is z  
-
-                ldi r1, 1
-                ldi r3, upOrDown
-                st r3, r1
-
-            else 
-
-                ldi r1, 0
-                ldi r3, upOrDown
-
-                st r3, r1
-
-            fi
-        fi
+        # Move ball back
+        ldi r2, calculate_ball_vertical
+        st r2, r2
     fi
 
-    ld r0, r1# в r1 окружающая среда
-    ldi r2, ball# шарик
-    ld r2, r2
     ldi r3, flush
+    ldi r0, ballAdress
+    ld r0, r0
+    ldi r2, ball
+    ld r2, r2
 
-    if
-        push r3
-        ldi r3, forwardOrBack
-        ld r3, r3
-        tst r3
-        pop r3
-    is z # если 0, то ->
+    ld r0, r1 # Env
 
-        shr r2
+    or r1, r2
 
-        if
-            move r0, r1
-            inc r1
-            ld r1, r1
-            tst r1
-        is pl
+    st r0, r2
+    st r3, r2 # flush
 
-            if
-                ldi r1, 0b00000001
-                cmp r2, r1
-            is eq
-
-                inc r0
-                ldi r2, 0b10000000
-
-            fi
-
-        fi
-
-        ldi r3, ball
-        st r3, r2
-
-    else 
-
-        shl r2
-
-        if
-            move r0, r1
-            dec r1
-            ld r1, r1
-            shr r1
-        is cc
-
-            if
-                ldi r1, 0b10000000
-                cmp r2, r1
-            is eq
-                
-                dec r0
-                ldi r2, 0b00000001
-
-            fi
-
-        fi
-
-        ldi r3, ball
-        st r3, r2
-
-    fi
-
-    if
-        push r1
-        ldi r1, upOrDown
-        ld r1, r1
-        tst r1
-        pop r1
-    is z
-
-        push r3
-        ldi r3, diagonal
-        ld r3, r3
-        sub r0, r3
-        move r3, r0
-        pop r3
-
-    else
-
-        push r3
-        ldi r3, diagonal
-        ld r3, r3
-        add r3, r0
-        pop r3
-
-    fi
-
-    ldi r3, ballAdress
-    st r3, r0
-    
+    st r0, r1
     popall
-
     rts
 
 define player_offset, 0x00
@@ -385,11 +246,10 @@ define upOrDown, 0x09
 define forwardOrBack, 0x0A
 define diagonal, 0x0B
 define ballAdress, 0x0C
-
-define turn_count, 0x10
-define turns, 0x11
+define calculate_ball_horizontal, 0x0D
+define calculate_ball_vertical, 0x0E
 
 define reset_keyboard, 0x5e
 define flush, 0x5f
 define display, 0x60
-	end
+    end
